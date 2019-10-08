a0f38a773af055d0dee2bad6a86759e4af25cf5c
Thienanh-682 <Thienanh-682@gmail.com>
2019-10-08 09:16:12 +0700

    class_poinr2D

--- Module2-session4/Point2D/index.php
+++ Module2-session4/Point2D/index.php
@@ -1,1 +1,8 @@
 <?php
+include_once("point2D.php");
+include_once("point3D.php");
+
+$point2D = new Point2D(3, 5);
+echo $point2D->toString() . "<br>";
+$point3D = new Point3D(4, 2, 5);
+echo $point3D->showPosition();
